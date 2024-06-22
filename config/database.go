d13bcc9caa52964c23e6dd8e2cd8285bb508fb67
Dewantara <46391302+GoodGuyzaxx@users.noreply.github.com>
2024-06-23 01:41:54 +0900

    Fix typo

--- config/database.go
+++ config/database.go
@@ -3,13 +3,13 @@ package config
 import (
 	"fmt"
 
-	"gorm.io/drive/mysql"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
-func loadDataBase() {
+func LoadDataBase() {
 
 	dbConnectionStr := fmt.Sprintf("%v:%v@tcp(%v)/%v?%v", ENV.DB_USERNAME, ENV.DB_PASSWORD, ENV.DB_URL, ENV.DB_DATABASE, "charset=utf8mb4&parseTime=True&loc=Local")
 	db, err := gorm.Open(mysql.Open(dbConnectionStr), &gorm.Config{})
@@ -17,5 +17,5 @@ func loadDataBase() {
 		panic("failed to connect database")
 	}
 
-	BD = db
+	DB = db
 }
