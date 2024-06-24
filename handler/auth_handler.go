257a47043f01f36bc2513ca150fe36294752af53
Dewantara <46391302+GoodGuyzaxx@users.noreply.github.com>
2024-06-24 22:25:04 +0900

    Login API Now Working

--- handler/auth_handler.go
+++ handler/auth_handler.go
@@ -40,3 +40,27 @@ func (h *authHandler) Register(c *gin.Context) {
 
 	c.JSON(http.StatusCreated, res)
 }
+
+func (h *authHandler) Login(c *gin.Context) {
+	var login dto.LoginRequest
+
+	err := c.ShouldBindJSON(&login)
+	if err != nil {
+		errorhandler.HandlerError(c, &errorhandler.BadRequestError{Message: err.Error()})
+		return
+	}
+
+	result, err := h.service.Login(&login)
+	if err != nil {
+		errorhandler.HandlerError(c, err)
+		return
+	}
+
+	res := helper.Response(dto.ResponseParams{
+		StatusCode: http.StatusOK,
+		Message:    "Berhasil Login",
+		Data:       result,
+	})
+
+	c.JSON(http.StatusOK, res)
+}
--- helper/password.go
+++ helper/password.go
@@ -6,3 +6,8 @@ func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	return string(bytes), err
 }
+
+func VerifyPassword(hashPassword string, password string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(hashPassword), []byte(password))
+	return err
+}
